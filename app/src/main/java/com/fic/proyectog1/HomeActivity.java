fc11b38d7b3102ce9710354883df1132026ef575
Alexis Moises Montaño Araujo <alexis.montano@info.uas.edu.mx>
2022-11-15 18:00:09 -0700

    Sesión 15-11-2022

--- app/src/main/java/com/fic/proyectog1/HomeActivity.java
+++ app/src/main/java/com/fic/proyectog1/HomeActivity.java
@@ -5,21 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button btnVisualizarSitioWeb,btnMarcarNumero;
+    Button btnVisualizarSitioWeb,btnMarcarNumero, btnVisualizarConfiguracionInalambrica;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         btnVisualizarSitioWeb = findViewById(R.id.btnVisualizarSitioWeb);
         btnMarcarNumero = findViewById(R.id.btnMarcarNumero);
+        btnVisualizarConfiguracionInalambrica = findViewById(R.id.btnVisualizarConfiguracionInalambrica);
 
         btnVisualizarSitioWeb.setOnClickListener(accionVisualizarSitioWeb);
         btnMarcarNumero.setOnClickListener(accionMarcarNumero);
+        btnVisualizarConfiguracionInalambrica.setOnClickListener(accionVisualizarConfiguracionInalambrica);
     }
 
     public View.OnClickListener accionVisualizarSitioWeb = new View.OnClickListener() {
@@ -36,6 +39,13 @@ public class HomeActivity extends AppCompatActivity {
         }
     };
 
+    public View.OnClickListener accionVisualizarConfiguracionInalambrica = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mostrarConfiguracionRedInalambrica();
+        }
+    };
+
     public void visualizarSitioWeb(String sitioWeb){
         Uri uri = Uri.parse(sitioWeb);
         Intent intencionVisualizarSitioWeb = new Intent(Intent.ACTION_VIEW, uri);
@@ -46,5 +56,10 @@ public class HomeActivity extends AppCompatActivity {
         Uri uri = Uri.parse("tel:" + numeroTelefono);
         Intent intencionMarcarNumero = new Intent(Intent.ACTION_DIAL,uri);
         startActivity(intencionMarcarNumero);
+    }
+
+    public void mostrarConfiguracionRedInalambrica(){
+        Intent intencionInalambrica = new Intent(Settings.ACTION_WIFI_SETTINGS);
+        startActivity(intencionInalambrica);
     }
 }
